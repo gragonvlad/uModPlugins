557da4ef66c7edfebed5cf50961c68a850f763b4
Iv Misticos <21005901+IvMisticos@users.noreply.github.com>
2019-12-05 21:55:02 +0200

    Update fix for No Green

--- uMod Plugins/NoGreen.cs
+++ uMod Plugins/NoGreen.cs
@@ -1,87 +1,93 @@
 using System;
+using CompanionServer;
 using ConVar;
 using Facepunch;
 using Facepunch.Math;
 using UnityEngine;
-using Time = UnityEngine.Time;
 
 namespace Oxide.Plugins
 {
-    [Info("No Green", "Iv Misticos", "1.3.7")]
+    [Info("No Green", "Iv Misticos", "1.3.8")]
     [Description("Remove admins' green names")]
     class NoGreen : RustPlugin
     {
-        private bool OnPlayerChat(ConsoleSystem.Arg arg, Chat.ChatChannel channel)
-        {
-            var player = arg.Player();
-            var text = arg.GetString(0, "text").Replace("\n", "").Replace("\r", "").Trim().EscapeRichText();
-            
-            if (Chat.serverlog)
-            {
-                ServerConsole.PrintColoured(ConsoleColor.DarkYellow, string.Concat("[", channel.ToString(), "] ", player.displayName, ": "), ConsoleColor.DarkGreen, text);
-                DebugEx.Log(channel == Chat.ChatChannel.Team
-                    ? $"[TEAM CHAT] {player} : {text}"
-                    : $"[CHAT] {player} : {text}");
-            }
-            
-            var color = "#5af";
-            var displayName = player.displayName.EscapeRichText();
-            
-            player.NextChatTime = Time.realtimeSinceStartup + 1.5f;
-            
-            var chatEntry = new Chat.ChatEntry
-            {
-                Channel = channel,
-                Message = text,
-                UserId = player.UserIDString,
-                Username = player.displayName,
-                Color = color,
-                Time = Epoch.Current
-            };
-            
-            RCon.Broadcast(RCon.LogType.Chat, chatEntry);
-            
-            if (channel != Chat.ChatChannel.Global)
-            {
-                if (channel == Chat.ChatChannel.Team)
-                {
-                    var team = arg.Player().Team;
-                    var list = team?.GetOnlineMemberConnections();
-                    if (list == null)
-                    {
-                        return false;
-                    }
-                    
-                    ConsoleNetwork.SendClientCommand(list, "chat.add2", new object[]
-                    {
-                        1,
-                        player.userID,
-                        text,
-                        displayName,
-                        color,
-                        1f
-                    });
-                    
-                    return false;
-                }
-            }
-            else if (ConVar.Server.globalchat)
-            {
-                ConsoleNetwork.BroadcastToAllClients("chat.add2", 0, player.userID, text, displayName, color, 1f);
-                return false;
-            }
-            
-            var radius = 2500f;
-            foreach (var basePlayer2 in BasePlayer.activePlayerList)
-            {
-                var sqrMagnitude = (basePlayer2.transform.position - player.transform.position).sqrMagnitude;
-                if (sqrMagnitude <= radius)
-                {
-                    ConsoleNetwork.SendClientCommand(basePlayer2.net.connection, "chat.add2", 0, player.userID, text, displayName, color, Mathf.Clamp01(radius - sqrMagnitude + 0.2f));
-                }
-            }
+	    private bool OnPlayerChat(BasePlayer player, string message, Chat.ChatChannel channel)
+	    {
+		    if (Chat.serverlog)
+		    {
+			    ServerConsole.PrintColoured(ConsoleColor.DarkYellow,
+				    string.Concat("[", channel, "] ", player.displayName, ": "), ConsoleColor.DarkGreen, message);
+			    
+			    var str = (player != null ? player.ToString() : null) ??
+			              $"{player.displayName}[{player.userID}]";
+			    
+			    if (channel == Chat.ChatChannel.Team)
+			    {
+				    DebugEx.Log("[TEAM CHAT] " + str + " : " + message);
+			    }
+			    else
+			    {
+				    DebugEx.Log("[CHAT] " + str + " : " + message);
+			    }
+		    }
 
-            return false;
-        }
+		    var color = "#5af";
+		    var displayName = player.displayName.EscapeRichText();
+		    var chatEntry = new Chat.ChatEntry
+		    {
+			    Channel = channel,
+			    Message = message,
+			    UserId = player.UserIDString,
+			    Username = displayName,
+			    Color = color,
+			    Time = Epoch.Current
+		    };
+
+		    RCon.Broadcast(RCon.LogType.Chat, chatEntry);
+		    if (channel != Chat.ChatChannel.Global)
+		    {
+			    if (channel == Chat.ChatChannel.Team)
+			    {
+				    var playerTeam = RelationshipManager.Instance.FindPlayersTeam(player.userID);
+				    if (playerTeam == null)
+				    {
+					    return false;
+				    }
+
+				    var onlineMemberConnections = playerTeam.GetOnlineMemberConnections();
+				    if (onlineMemberConnections != null)
+				    {
+					    ConsoleNetwork.SendClientCommand(onlineMemberConnections, "chat.add2", 1, player.userID,
+						    message, displayName, color, 1f);
+				    }
+
+				    playerTeam.BroadcastTeamChat(player.userID, displayName, message, color);
+				    return true;
+			    }
+		    }
+		    else if (ConVar.Server.globalchat)
+		    {
+			    ConsoleNetwork.BroadcastToAllClients("chat.add2", 0, player.userID, message, displayName, color, 1f);
+			    return true;
+		    }
+
+		    if (player == null)
+			    return true;
+		    
+		    var radius = 2500f;
+		    foreach (var basePlayer in BasePlayer.activePlayerList)
+		    {
+			    var sqrMagnitude = (basePlayer.transform.position - player.transform.position).sqrMagnitude;
+			    if (sqrMagnitude <= radius)
+			    {
+				    ConsoleNetwork.SendClientCommand(basePlayer.net.connection, "chat.add2", 0, player.userID,
+					    message,
+					    displayName, color, Mathf.Clamp01(radius - sqrMagnitude + 0.2f));
+			    }
+		    }
+
+		    return true;
+
+	    }
     }
 }
